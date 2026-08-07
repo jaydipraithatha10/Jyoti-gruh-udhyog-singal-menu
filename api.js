7fe6d709301f67a851c3c5b5ac4de9a886ab7a0d
jaydipraithatha10 <jaydipraithatha10@gmail.com>
2026-08-08 02:30:03 +0530

    Update api.js

--- api.js
+++ api.js
@@ -157,18 +157,20 @@ const products = category.products.filter(product =>
     });
 
     
-}document.getElementById("productCount").innerText = totalProducts;
-
-if(totalProducts === 0){
-
-    menuContainer.innerHTML = `
-        <div class="no-products">
-            🔍<br><br>
-            No Products Found
-        </div>
-    `;
-
 }
+
+    document.getElementById("productCount").innerText = totalProducts;
+
+    if(totalProducts === 0){
+
+        menuContainer.innerHTML = `
+            <div class="no-products">
+                🔍<br><br>
+                No Products Found
+            </div>
+        `;
+
+    }
 
 }
 
